da7383f517a09c16fddaf2bcef1ec94605f3b070
Leon Gonzalez <leonesteban@gmail.com>
2021-12-02 19:05:55 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const cowsay = require('cowsay')
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/cowsay', (req, res) => {
+    text = req.query.message
+    if(!text){
+        return res.send({
+            error: 'Por favor especifica un mensaje'
+        })
+    }
+
+    console.log('Text received: ' + text)
+    res.send(cowsay.say({
+        text,
+        e: "O O",
+        T: "U"
+    }))
+})
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+// console.log(cowsay.say({
+//     text: "I am a cow",
+//     e: "O O",
+//     T: "U"
+// }))
